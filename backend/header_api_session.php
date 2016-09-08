6cb7014a7c9d406733a51eb0b470ed8eb07adda1
Henryzhao96 <shzhaotc@gmail.com>
2016-09-09 01:09:20 +0800

    Sync File from Server and fix bugs in session api

--- backend/header_api_session.php
+++ backend/header_api_session.php
@@ -1,31 +1,29 @@
 <?php
 //Environment settings
 $iapp_id = 'iapp55501'; //Eg. iapp40000
-$iapp_bupt_url = 'http://yiban.bupt.edu.cn/welcome/index.php'; //Eg. http://yiban.bupt.edu.cn/askforleave/leave.php
+$iapp_bupt_url = 'http://www.buptyiban.org/welcome/index.php'; //Eg. http://yiban.bupt.edu.cn/askforleave/leave.php
 
+//Program
+//Set SESSION Cookie's expire time and path before session_start()
+session_set_cookie_params(86400,substr(
+    getcwd(),
+    strpos(getcwd(),'html')+5,
+    (strpos(getcwd(),'/',strpos(getcwd(),'html')+5) ? strpos(getcwd(),'/',strpos(getcwd(),'html')+5) - (strpos(getcwd(),'html')+5) : 100)
+));
 session_start();
-if(isset($_SESSION['create_time']) and (time() - $_SESSION['create_time'] > 12*60*60))
-    session_reset();
-if(session_status() == PHP_SESSION_ACTIVE and $_SESSION['current_iapp'] != $iapp_id)
-{
-    session_reset();
-    $_SESSION['current_iapp'] = $iapp_id;
-    header("Location:".$iapp_bupt_url);
-    exit;
-}
+//Check the auth
 if(!isset($_GET['verify_request']) and !isset($_SESSION['verify_request']))
 {
     header("Location:http://f.yiban.cn/".$iapp_id);
     exit;
 }
+//Storage encrypted string into session
 if(isset($_GET["verify_request"]))
 {
-    $_SESSION['create_time'] = time();
     $_SESSION['verify_request'] = $_GET['verify_request'];
     $_SESSION['yb_uid'] = $_GET['yb_uid'];
-    header("Location:".$iapp_bupt_url);
+    header("Location:".$iapp_bupt_url); //Jump again to remove uri
     exit;
 }
 $_REQUEST['verify_request'] = $_SESSION['verify_request'];
 $_REQUEST['yb_uid'] = $_SESSION['yb_uid'];
-?>
